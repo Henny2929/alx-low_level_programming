92306d870b172c01134e497b6e07e216d0ffaecb
Henny2929 <eniolaafuwape@gmail.com>
2023-06-17 18:32:19 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program thats prints line
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fput("and that place of art is useful\" -Dora Koorpar, 2015-10-19\n", stdout);
+return (1);
+}
